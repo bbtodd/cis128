6da2ed0ddea63b36fca918919152765057214531
bbtodd <47540445+bbtodd@users.noreply.github.com>
2024-02-12 20:11:47 -0800

    Update scripts.js

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,29 @@
-//test
+    $(document).ready(function(){
+        $(".hdr-animation").click(function(){
+            $( "header h1" ).animate({
+                fontSize: "3em",
+                color: "blue",
+            }, 1500, function(){
+                console.log("done");
+                $( "header h1" ).animate({
+                    fontSize: "2em",
+                    color: "black",
+                }, 1500);
+            });
+
+            $( ".bio-pic" ).animate({
+                borderWidth: "4px",
+            }, 500 );
+
+            $( "header h1" ).css({
+                color: "dodgerblue",
+            });
+
+            setTimeout(changeH1toBlack, 3000);
+        });
+
+        function changeH1toBlack() {
+            document.getElementsByTagName("h1")[0].style.color = "black";
+        }
+    
+    });
